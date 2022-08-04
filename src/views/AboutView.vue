fecc343b2193bcfd7ef1208ec5c491c2774e9b74
laroca <laroca73@gmail.com>
2022-08-04 19:06:17 +0200

    Update Lab Exercices to Practice

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Exercices Lessons</h1>
+    <article>
+      <button v-if="!signedIn">V-if Log In</button>
+      <button v-if="signedIn">V-if Log Out</button>
+    </article>
+      <article>
+      <button v-if="!signedIn">V-if Log In</button>
+      <button v-else>V-else Log Out</button>
+    </article>
+     <article>
+    <button v-show="!signedIn">V-show Log In</button>
+    <button v-show="signedIn">V-show Log Out</button>
+  </article>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      signedIn:true
+    }
+  }
+}
+</script>
